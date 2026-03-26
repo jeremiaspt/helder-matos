e3aa1479f39c621a9c57478062583bd9d3fe9e30
jeremiaspt <85419158+jeremiaspt@users.noreply.github.com>
2026-03-26 19:29:35 +0000

    service: "gmail",

--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,11 @@ app.post("/ticket", async (req,res)=>{
  try{
 
  const transporter = nodemailer.createTransport({
-
-  host:"smtp.gmail.com",
-  port:465,
-  secure:true,
-
+  service: "gmail",
   auth:{
-   user:process.env.EMAIL,
-   pass:process.env.PASS
+   user: process.env.EMAIL,
+   pass: process.env.PASS
   }
-
  })
 
  await transporter.sendMail({
