34c0162722fd35b222085fe68a90a9337d2206c3
jeremiaspt <85419158+jeremiaspt@users.noreply.github.com>
2026-03-26 20:30:10 +0000

    app cors update

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,23 @@ const app = express()
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-app.use(cors())
+app.use(cors({
+ origin: "https://helder-matos.vercel.app",
+ methods: ["GET","POST","OPTIONS"],
+ allowedHeaders: ["Content-Type"]
+}))
+
+app.options("*", cors())
+
 app.use(express.json())
 
 app.post("/ticket", async (req,res)=>{
-
- console.log("BODY:",req.body)
 
  const {name,phone,email,city,description} = req.body
 
  try{
 
- const response = await resend.emails.send({
+ await resend.emails.send({
 
  from: "Assistência <onboarding@resend.dev>",
  to: process.env.EMAIL,
@@ -36,13 +41,11 @@ app.post("/ticket", async (req,res)=>{
  `
  })
 
- console.log("EMAIL RESPONSE:",response)
-
  res.json({status:"ok"})
 
  }catch(err){
 
- console.log("EMAIL ERROR:",err)
+ console.log(err)
 
  res.status(500).json({error:"erro envio email"})
 
@@ -53,9 +56,5 @@ app.post("/ticket", async (req,res)=>{
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, "0.0.0.0", () => {
- console.log("Server running on port", PORT)
-})
-
-app.get("/", (req,res)=>{
- res.send("API ONLINE")
+ console.log("Server running on", PORT)
 })
