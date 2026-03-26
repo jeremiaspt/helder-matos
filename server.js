c60fcfa4a6916a033ad1caff4c627cf9ecb51a92
jeremiaspt <85419158+jeremiaspt@users.noreply.github.com>
2026-03-26 20:48:12 +0000

    update

--- server.js
+++ server.js
@@ -60,3 +60,7 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, "0.0.0.0", ()=>{
   console.log("Server running on port", PORT)
 })
+
+app.get("/", (req,res)=>{
+  res.send("API ONLINE")
+})
