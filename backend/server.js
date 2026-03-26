793ea80aaae732b24c4bf637ca55d86b4722a819
jeremiaspt <85419158+jeremiaspt@users.noreply.github.com>
2026-03-26 18:15:33 +0000

    first commit

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const cors = require("cors")
+const nodemailer = require("nodemailer")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.post("/ticket", async (req,res)=>{
+
+ const {name,phone,email,city,description} = req.body
+
+ try{
+
+ const transporter = nodemailer.createTransport({
+  service:"gmail",
+  auth:{
+   user:process.env.EMAIL,
+   pass:process.env.PASS
+  }
+ })
+
+ await transporter.sendMail({
+
+  from:process.env.EMAIL,
+  to:process.env.EMAIL,
+  subject:"Novo pedido de assistência",
+
+  text:`
+Nome: ${name}
+Telefone: ${phone}
+Email: ${email}
+Localidade: ${city}
+
+Descrição:
+${description}
+`
+ })
+
+ res.json({status:"ok"})
+
+ }catch(err){
+
+ res.status(500).json({error:"erro envio email"})
+
+ }
+
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+ console.log("Servidor iniciado")
+})
