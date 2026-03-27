d2f3f981cb4b8fbf88890d878eb4b9c77b9df957
jeremiaspt <85419158+jeremiaspt@users.noreply.github.com>
2026-03-27 02:12:49 +0000

    admin update

--- api/updateTicket.js
+++ api/updateTicket.js
@@ -1,9 +1,9 @@
 export default async function handler(req,res){
 
- const { ticket, estado } = req.body
+ const { ticket, estado, obs } = req.body
 
  await fetch(
-  `${process.env.SHEET_WEBHOOK}?ticket=${ticket}&estado=${estado}`
+ `${process.env.SHEET_WEBHOOK}?ticket=${ticket}&estado=${estado}&obs=${encodeURIComponent(obs)}`
  )
 
  res.status(200).json({status:"ok"})
