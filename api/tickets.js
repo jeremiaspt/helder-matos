c9c813d62ae3c1ce95d3139f456343f93d9364d5
jeremiaspt <85419158+jeremiaspt@users.noreply.github.com>
2026-03-27 01:01:12 +0000

    admin pw

--- api/tickets.js
+++ api/tickets.js
@@ -1,9 +1,25 @@
 export default async function handler(req,res){
 
- const sheetUrl = process.env.SHEET_JSON
+ const auth = req.headers.authorization
 
- const r = await fetch(sheetUrl)
+ if(!auth){
+  res.setHeader("WWW-Authenticate","Basic")
+  return res.status(401).send("Authentication required")
+ }
 
+ const base64 = auth.split(" ")[1]
+ const decoded = Buffer.from(base64,"base64").toString()
+
+ const [user,pass] = decoded.split(":")
+
+ if(
+  user !== process.env.ADMIN_USER ||
+  pass !== process.env.ADMIN_PASS
+ ){
+  return res.status(401).send("Access denied")
+ }
+
+ const r = await fetch(process.env.SHEET_JSON)
  const data = await r.json()
 
  res.status(200).json(data)
